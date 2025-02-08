d6eb930b6a45cff76cd485e8e539ce338ec49a75
AlexMaschenko3110 <xander.maschenko3110@gmail.com>
2025-02-08 18:46:49 +0200

    init project

---
+++ data.py
@@ -0,0 +1,15 @@
+data = [
+    {
+        'question': 'Як перекласти кіт',
+        'right_answer': 'cat',
+        'wrong_answers': ['butterfly', 'plane', 'room']
+    },
+    {
+        'question': 'Як перекласти слон',
+        'right_answer': 'elephant',
+        'wrong_answers': ['giraffe', 'whale', 'dolphin']
+
+    }
+]
+
+number_data = 0
